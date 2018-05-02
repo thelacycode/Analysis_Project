f0fcf3b56ed36c87c4fff1edb1db5fd1ea0ceed6
Ty <thelacycode@gmail.com>
2018-05-01 22:37:25 -0400

    refactor code

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
 import psycopg2
 
@@ -5,39 +6,28 @@ app = Flask(__name__)
 
 DBNAME = "news"
 
-
 # create additional table views
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
 c.execute(
-        '''
-        CREATE TABLE errors(
-            day date,
-            err numeric
-        )
-        ''')
-c.executemany(
-        '''
-        INSERT INTO errors
+    '''
+    CREATE VIEW errors(
         SELECT(time::DATE) as day, count(status) as err
         FROM log
         WHERE status = '404 NOT FOUND'
-        Group by day        
-        ''')
+        Group by day
+    )
+    ''')
+
 c.execute(
-        '''
-        CREATE TABLE total_status(
-            day date, 
-            total_status numeric
-        )
-        ''')
-c.executemany(
-        '''
-        INSERT INTO total_status
+    '''
+    CREATE VIEW total_status(
         SELECT (time::DATE) as day, count(status) as total
         FROM log
         Group by day
-        ''')
+    )
+    ''')
+
 c.commit()
 db.close()
 
@@ -47,28 +37,28 @@ db.close()
 def questions():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    ques1 = '''select title, count(path) as num 
-                FROM articles 
-                join log on articles.slug = (REPLACE (log.path, '/article/', '')) 
-                group by title order by num desc limit 3; 
-                '''
+    ques1 = '''select title, count(path) as num
+            FROM articles
+            join log on articles.slug = (REPLACE (log.path, '/article/', ''))
+            group by title order by num desc limit 3;
+            '''
     c.execute(ques1)
     ans1 = c.fetchall()
 
-    ques2 = '''select name, count(path) as num 
-               FROM articles 
-               join log on articles.slug = (REPLACE(log.path, '/article/', '')) 
-               join authors on articles.author = authors.id 
-               group by name order by num desc limit 5
+    ques2 = '''select name, count(path) as num
+            FROM articles
+            join log on articles.slug = (REPLACE(log.path, '/article/', ''))
+            join authors on articles.author = authors.id
+            group by name order by num desc limit 5
                 '''
     c.execute(ques2)
     ans2 = c.fetchall()
 
-    ques3 = '''select errors.day, err, total, err/total as num 
-                    from errors
-                    join total_status on errors.day = total_status.day 
-                    where ((err/total)::decimal) > .01 
-                    order by day desc;
+    ques3 = '''select errors.day, err, total, err/total as num
+            from errors
+            join total_status on errors.day = total_status.day
+            where ((err/total)::decimal) > .01
+            order by day desc;
                     '''
     c.execute(ques3)
     ans3 = c.fetchall()
